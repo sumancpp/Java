ae8c25c180fbf3a3ecd6f9a9f856a8d14a262f52
Suman Maity <someone.cpp@proton.me>
2025-08-04 12:55:48 +0530

    Add files via upload

---
+++ Variables/DataTypes.java
@@ -0,0 +1,26 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+        // DataTypes are two types
+        // Primitive    Non-Primitive
+
+       /*  Primitive 
+          byte
+          short
+          char
+          boolean
+          int
+          long 
+          float 
+          double
+
+
+
+        Non-Primitive 
+           String
+           Array 
+           Class 
+           Object 
+           Interface  */ 
+    }
+}
