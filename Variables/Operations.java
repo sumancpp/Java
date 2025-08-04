43fed765000e59ebb71aad70f3d8cecad0f9f3d4
Suman Maity <someone.cpp@proton.me>
2025-08-05 00:46:48 +0530

    Add files via upload

---
+++ Variables/Operations.java
@@ -0,0 +1,15 @@
+public class Operations {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println("The sum is " + sum);
+        int diff = a - b;
+        System.out.println("The difference is " + diff);
+        int mul = a * b ; 
+        System.out.println("The multipication is " + mul);
+        int div = a / b;
+        System.out.println("The division is " + div);
+
+    }
+}
