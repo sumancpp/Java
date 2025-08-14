79d6cd5d2c69df35d5833749df60571db0ed91a9
Suman Maity <someone.cpp@proton.me>
2025-08-15 00:41:42 +0530

    Add files via upload

---
+++ Pattern Questions/Problem04.java
@@ -0,0 +1,12 @@
+// Inverted half pyramid
+
+public class Problem04 {
+    public static void main(String[] args) {
+        for(int i=4; i>=1; i--){
+          for(int j=1; j<=i; j++){
+            System.out.print("*");
+          }
+          System.out.println();
+        }
+    }
+}
