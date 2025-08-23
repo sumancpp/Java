639634eeef4ffc25689f17c12813b4c045444f06
Suman Maity <someone.cpp@proton.me>
2025-08-24 01:07:24 +0530

    Add files via upload

---
+++ Pattern Questions/Problem12.java
@@ -0,0 +1,18 @@
+public class Problem12 {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++) {
+           //spaces
+           for(int j=1; j<=5-i; j++) {
+               System.out.print(" ");
+           }
+
+
+           //numbers
+           for(int j=1; j<=i; j++) {
+               System.out.print(i+" ");
+           }
+           System.out.println();
+       }
+
+    }
+}
