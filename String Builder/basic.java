62f49703db8d43ebc92a5e15b07113ca27d626a7
Suman Maity <someone.cpp@proton.me>
2025-09-21 01:01:05 +0530

    Add files via upload

---
+++ String Builder/basic.java
@@ -0,0 +1,37 @@
+public class basic {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("uman");
+
+        //System.out.println(sb.charAt(0));
+
+        sb.setCharAt(0, 'P');
+        System.out.println(sb);
+
+    //  uman ----> Suman
+    
+        // sb.insert(0, 'S');
+        // System.out.println(sb);
+
+        // sb.insert(3, 'a');
+        // System.out.println(sb);
+
+    // delete the extra a
+
+    //sb.delete(3, 4);
+    //System.out.println(sb);
+
+    // sb.delete(3, 5);
+    // System.out.println(sb);
+
+    // sb.append(" ");
+    // sb.append("M");
+    // sb.append("a");
+    // sb.append("i");
+    // sb.append("t");
+    // sb.append("y");
+
+    // System.out.println(sb);
+    // System.out.println(sb.length());
+        
+    }
+}
