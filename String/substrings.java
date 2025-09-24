627e37ed11f3f92925e545ced62b4c512d9b54f1
Suman Maity <someone.cpp@proton.me>
2025-09-25 00:07:36 +0530

    Add files via upload

---
+++ String/substrings.java
@@ -0,0 +1,8 @@
+public class substrings {
+    public static void main(String[] args) {
+        String name = "TonyStark";
+     
+       System.out.println(name.substring(0, 4));
+       System.out.println(name.substring(3));
+    }
+}
