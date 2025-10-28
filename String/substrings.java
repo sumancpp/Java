2358093f3dda7b37394eb4228c24e37e37513a67
Suman Maity <someone.cpp@proton.me>
2025-10-28 23:17:35 +0530

    Update substrings.java

--- String/substrings.java
+++ String/substrings.java
@@ -1,8 +1,10 @@
 public class substrings {
     public static void main(String[] args) {
+
         String name = "TonyStark";
      
        System.out.println(name.substring(0, 4));
        System.out.println(name.substring(3));
     }
+
 }
