366dfba4050d7299288fde416645c6ea04140c07
Suman Maity <someone.cpp@proton.me>
2025-10-31 00:44:59 +0530

    Add files via upload

---
+++ Recursion/calculatesumofNnum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class calculatesumofNnum {
+    public static void calsum(int num, int sum , int st){
+        if(st == num){
+            sum += st;
+            System.out.println("The sum is "+sum);
+            return;
+        }
+      sum = sum + st;
+      calsum(num, sum, st + 1);
+    }
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum = 0;
+        int st = 1;
+        calsum(num, sum, st);
+        sc.close();
+    }
+}
