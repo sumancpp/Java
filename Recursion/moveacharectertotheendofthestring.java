8918f24e4bc39263f225befac6d0ee71c1a9617f
Suman Maity <someone.cpp@proton.me>
2025-11-10 18:52:37 +0530

    Add files via upload

---
+++ Recursion/moveacharectertotheendofthestring.java
@@ -0,0 +1,26 @@
+// Move all 'x' to the end of the String===> 'axbcxxd'
+
+public class moveacharectertotheendofthestring {
+    public static void moveAllX(String str, int idx, int count, String newString){
+        if(idx == str.length()){
+            for(int i=0; i<=count; i++){
+                newString += 'x';
+            }
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(currChar == 'x'){
+            count++;
+            moveAllX(str, idx+1, count, newString);
+        }else{
+            newString += str.charAt(idx);
+            moveAllX(str, idx+1, count, newString);
+        }
+        
+    }
+    public static void main(String[] args) {
+        String str = "axbcxxd";
+        moveAllX(str, 0, 0, "");
+    }
+}
