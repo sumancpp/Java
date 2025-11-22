39a1c37e01bc18c4ca7c040bafd022d1949fd9a0
Suman Maity <someone.cpp@proton.me>
2025-11-23 01:48:16 +0530

    Add files via upload

---
+++ Recursion/uniquesubsequenceofastring.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+
+public class uniquesubsequenceofastring {
+
+    public static void printSubseq(String str, int idx, String res, HashSet<String> allSubseq) {
+        if (idx == str.length()) {
+            if (allSubseq.contains(res)) {
+                return;
+            }
+            allSubseq.add(res);
+            System.out.println(res);
+            return;
+        }
+
+        // choose
+        printSubseq(str, idx + 1, res + str.charAt(idx), allSubseq);
+        // don't choose
+        printSubseq(str, idx + 1, res, allSubseq);
+    }
+
+    public static void main(String[] args) {
+
+        String str1 = "abc";
+        String str2 = "aaa";
+        HashSet<String> allSubseq = new HashSet<>(); // a set that holds unique strings
+        printSubseq(str2, 0, "", allSubseq);
+        printSubseq(str1, 0, "", allSubseq);
+
+    }
+}
