d2c295126f8723eb7e95b1a8e8aa840c6096b954
Suman Maity <someone.cpp@proton.me>
2025-12-16 01:09:28 +0530

    Add files via upload

--- Recursion/calxpowern.java
+++ Recursion/calxpowern.java
@@ -6,13 +6,12 @@ public class calxpowern {
         if(x==0){
             return 0;
         }
-        int xpownm1 = calculate(x, n-1);
-        int xpown = x * xpownm1;
+        int xpown = x * calculate(x, n-1); 
         return xpown;
         
     }
     public static void main(String[] args) {
-        int x=2, n=3;
+        int x=0, n=3;
         int ans = calculate(x, n);
         System.out.println(ans);
     }
