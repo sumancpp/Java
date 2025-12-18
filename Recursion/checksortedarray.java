dd0ddf999607faa9b20980a7755fdaf164e7167d
Suman Maity <someone.cpp@proton.me>
2025-12-19 02:33:51 +0530

    Add files via upload

---
+++ Recursion/checksortedarray.java
@@ -0,0 +1,20 @@
+// Strictly Increasing 
+// ex:- 1 2 3 4 [right]
+// ex:- 1 2 3 3 4 [Wrong]
+
+public class checksortedarray {
+    public static boolean check(int arr[], int idx){
+        if(idx==arr.length-1){
+            return true;
+        }
+        if(arr[idx] < arr[idx + 1]){
+          return check(arr, idx+1);
+        }else{
+           return false;
+        }
+    }
+    public static void main(String[] args) {
+        int arr [] = {1, 2, 3};
+        System.out.println(check(arr, 0));
+    }
+}
