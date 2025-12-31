e6de64d6d78e58d6eac67b087a80c22dacecacb0
Suman Maity <someone.cpp@proton.me>
2026-01-01 00:14:26 +0530

    Add files via upload

---
+++ Object Oriented  Programmimg/Inheritence.java
@@ -0,0 +1,31 @@
+class Shape{
+    public void area(){
+        System.out.println("Displaying... Area!");
+    }
+
+    class Triangle extends Shape {
+        public void area(int l, int h){
+        System.out.println(0.5 * l * h);
+        }
+    }
+
+    class Square extends Shape {
+        public void area(int l, int h){
+            System.out.println(l*h);
+        }
+    }
+}
+
+public class Inheritence {
+    public static void main(String[] args) {
+        Shape s1 = new Shape();
+
+        s1.area();
+
+        Shape.Square s2 = s1.new Square();
+         s2.area(4,5);
+
+         Shape.Triangle t1 = s1.new Triangle();
+         t1.area(2,3);
+    }
+}
