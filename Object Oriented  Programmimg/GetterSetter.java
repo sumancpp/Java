75aae808a11999acee025d357bd426a29476ba07
Suman Maity <someone.cpp@proton.me>
2026-01-02 12:26:26 +0530

    Add files via upload

---
+++ Object Oriented  Programmimg/GetterSetter.java
@@ -0,0 +1,27 @@
+class Account{
+    public String name;
+    protected String email;
+    private String password;
+    
+
+    // getter & setter
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public void setPassword(String pass){
+         this.password = pass;
+    }
+
+}
+
+public class GetterSetter {
+    public static void main(String[] args) {
+        Account ac = new Account();
+        ac.name = "Suman";
+        ac.email = "suman@gmail.com";
+        ac.setPassword("AbC123");
+        System.out.println(ac.getPassword());
+    }
+}
