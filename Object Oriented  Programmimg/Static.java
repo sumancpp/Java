f66e446527949904207eaf7f44803e230182f6ab
Suman Maity <someone.cpp@proton.me>
2026-01-15 18:39:28 +0530

    Add files via upload

---
+++ Object Oriented  Programmimg/Static.java
@@ -0,0 +1,20 @@
+class Student {
+    String name;
+    static String collage;
+
+ public static void changeClg(){
+    collage = "New Collage";
+    System.out.println(collage);
+ }
+}
+
+public class Static {
+    public static void main(String[] args) {
+        Student.collage = "omdayal";
+        Student s1 = new Student();
+        System.out.println(s1.collage);
+        s1.changeClg();
+        s1.name = "Suman";
+        System.out.println(s1.name);
+    }
+}
