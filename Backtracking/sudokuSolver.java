3a2f9da71ba745c80b48e7ec71eb4577b4a2e054
Suman Maity <someone.cpp@proton.me>
2026-03-17 22:59:41 +0530

    Update sudokuSolver.java

--- Backtracking/sudokuSolver.java
+++ Backtracking/sudokuSolver.java
@@ -89,8 +89,9 @@ public class sudokuSolver {
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
                 System.out.print(board[i][j] + " ");
-            }
+            
             System.out.println();
+
         }
     }
 }
